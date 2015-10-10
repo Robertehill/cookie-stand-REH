8191608e1e09146c9ccbcc28c4468d5c69ebed76
Robert Hill <codingbyrobert@gmail.com>
2015-10-10 12:55:55 -0700

    Replace store and remove store working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var listOfStands = [];//list array to check if a store can be replaced
 var CookieStand = function(standLoc, minCust, maxCust, avgCookiePerCust) {
   this.standLoc = standLoc;
   this.minCust = minCust;
@@ -5,6 +6,7 @@ var CookieStand = function(standLoc, minCust, maxCust, avgCookiePerCust) {
   this.avgCookiePerCust = avgCookiePerCust;
   this.hour = [];
   this.cookieTotal = 0;
+  listOfStands.push(this.standLoc.toLowerCase());
 
   this.randCustPerHour = function(){
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
@@ -27,40 +29,45 @@ var CookieStand = function(standLoc, minCust, maxCust, avgCookiePerCust) {
   };
   //called here so it adds values to the hour array and saves the total when an intance of the object is created
   this.totalCookiePerDay();
-
   function makeTable(loc, array, total) {
     //Make new table row with the name of the store
-    var place = document.getElementById('sales');
+    var table = document.getElementById('sales');
     var row = document.createElement('tr');
     row.appendChild(document.createTextNode(loc));
-    place.appendChild(row);
+    table.appendChild(row);
+    row.id = loc.toLowerCase();
 
     //add random hourly numbers to table
     for(var i = 0; i < array.length; i++) {
-        var hour = row;
         var data = document.createElement('td');
         data.appendChild(document.createTextNode(array[i]));
-        hour.appendChild(data);
+        row.appendChild(data);
       };
     // Add total cookies to table
     var totalData = document.createElement('td');
     totalData.appendChild(document.createTextNode(total));
-    hour.appendChild(totalData);
+    row.appendChild(totalData);
+
   }
   //call makeTable here to pass values to the html table when an instance of the object is created
   makeTable(this.standLoc, this.hour, this.cookieTotal);
-
-  //even rows gets shaded
+ //even rows get shaded
   var table = document.getElementById("sales");
   var rows = table.getElementsByTagName("tr");
+
   for(i = 0; i < rows.length; i++){
       if(i % 2 == 0){
           rows[i].className = "even";
       }
+      else{
+        rows[i].className = "odd";
+      }
+
   };
+
 }
-//hard wired stores
-var pikePlace = new CookieStand('Pike Place Markert', 17, 88, 5.2 );
+//hard wired stores. Need to push to list array here to avoid duplicates when replacing.
+var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2 );
 var seaTac = new CookieStand('SeaTac Airport', 6, 44, 1.2 );
 var southcenter = new CookieStand('Southcenter Mall', 11, 38, 1.9);
 var bellSq = new CookieStand('Bellevue Square', 20, 48, 3.3);
@@ -83,17 +90,53 @@ var storeSubmit = function(e) {
   }
   //check the make sure min is smaller than max
   else if(minCust.value > maxCust.value){
-    return alert('Please make sure the Minimum Customers is a smaller number than Maximum Customers');
+      return alert('Please make sure the Minimum Customers is a smaller number than Maximum Customers');
   }
-  else
-  //here for debuging
+  //start of replace check
+  for (var i = 0; i < listOfStands.length; i++) {
+      if(listOfStands[i] != null){
+        if(listOfStands[i].toLowerCase() === storeLoc.value.toLowerCase()){
+          var replace = confirm('This stand location already exists would you like to replace it?');
+          if (replace){
+            var parent = document.getElementById('sales');
+            var child = document.getElementById(storeLoc.value.toLowerCase());
+            parent.removeChild(child);
+            // console.log(storeLoc.value.toLowerCase() +' "replace" if statement');
+            // replace list array value here
+            listOfStands.splice( i, 1 );
+            var newstore = new CookieStand(storeLoc.value, minCust.value, maxCust.value, avgCookies.value);
+            storeLoc.value = null;
+            minCust.value = null;
+            maxCust.value = null;
+            avgCookies.value = null;
+            // return to stop "for" loop
+            return;
+          }
+          else{
+            //create newstore here if the user choose not to replace store
+            var newstore = new CookieStand(storeLoc.value, minCust.value, maxCust.value, avgCookies.value);
+            listOfStands.push(newstore.standLoc.toLowerCase());
+            storeLoc.value = null;
+            minCust.value = null;
+            maxCust.value = null;
+            avgCookies.value = null;
+            // return to stop "for" loop
+            return;
+          }
+
+        }
+      }
+  };
+  //end of replace section
+
+  //create new store and push to list array
+  var newstore = new CookieStand(storeLoc.value, minCust.value, maxCust.value, avgCookies.value);
+//here for debugging
   console.log('new store location = ' + storeLoc.value);
   console.log('min customers for ' + storeLoc.value +' = ' + minCust.value);
   console.log('max customers for ' + storeLoc.value +' = ' +maxCust.value);
   console.log('avg cookie per customer for ' + storeLoc.value +' = ' +avgCookies.value);
   //end debug
-  var newstore = new CookieStand(storeLoc.value, minCust.value, maxCust.value, avgCookies.value);
-
   //clear all input boxes here to prevent duplicate stores on table
   storeLoc.value = null;
   minCust.value = null;
@@ -101,10 +144,34 @@ var storeSubmit = function(e) {
   avgCookies.value = null;
 
 };
-var addButton = document.getElementById("addButton");
-addButton.addEventListener('click', storeSubmit);
-//end form/button area
+//Start of remove section
+var removeStore = function(e) {
+  e.preventDefault();
+  var remove = document.getElementById('removeInput');
 
+  for (var i = 0; i < listOfStands.length; i++) {
+    if(listOfStands[i] != null){
+      if(listOfStands[i].toLowerCase() === remove.value.toLowerCase()){
+        console.log('Have target to remove');
+        var parent = document.getElementById('sales');
+        var child = document.getElementById(remove.value.toLowerCase());
+        parent.removeChild(child);
+        listOfStands.splice( i, 1 );
+        remove.value = null;
+        return;
+
+      }
+
+    }
+  }
+}
+//end remove section
+//buttons
+var addButton = document.getElementById('addButton');
+addButton.addEventListener('click', storeSubmit);
+var removeButton = document.getElementById('removeButton');
+removeButton.addEventListener('click', removeStore);
+//end form/button area
 
 
 
